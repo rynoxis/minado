7f8685e9eca83532237776ea834c9e7cc123fb81
rynoxis <goldenpath11090@gmail.com>
2022-08-23 11:28:38 -0400

    Update welcome view.

---
+++ web/src/components/WelcomeView.vue
@@ -0,0 +1,79 @@
+<template>
+    <main aria-labelledby="profile-overview-title">
+        <div class="rounded-lg bg-white overflow-hidden shadow">
+            <h2 class="sr-only" id="profile-overview-title">Profile Overview</h2>
+            <div class="bg-white p-6">
+                <div class="sm:flex sm:items-center sm:justify-between">
+                    <div class="sm:flex sm:space-x-5">
+                        <div class="flex-shrink-0">
+                            <img
+                                class="mx-auto h-20 w-20 rounded-full"
+                                src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                alt=""
+                            />
+                        </div>
+                        <div class="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
+                            <p class="text-sm font-medium text-gray-600">Nexa Rocks!</p>
+                            <p class="text-xl font-bold text-gray-900 sm:text-2xl">Welcome Guest</p>
+                            <p class="text-sm font-medium text-gray-600">Human Resources Manager</p>
+                        </div>
+                    </div>
+                    <div class="mt-5 flex justify-center sm:mt-0">
+                        <a href="#" class="flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"> View profile </a>
+                    </div>
+                </div>
+            </div>
+            <div class="border-t border-gray-200 bg-gray-50 grid grid-cols-1 divide-y divide-gray-200 sm:grid-cols-3 sm:divide-y-0 sm:divide-x">
+                <div class="flex flex-col px-6 py-5 text-sm font-medium text-center">
+                    <span class="text-2xl text-gray-900">
+                        1.2MH/s
+                    </span>
+
+                    <span class="text-gray-600">
+                        Hash Rate
+                    </span>
+                </div>
+
+                <div class="flex flex-col px-6 py-5 text-sm font-medium text-center">
+                    <span class="text-2xl text-gray-900">
+                        4
+                    </span>
+
+                    <span class="text-gray-600">
+                        Active Miners
+                    </span>
+                </div>
+
+                <div class="flex flex-col px-6 py-5 text-sm font-medium text-center">
+                    <span class="text-2xl text-gray-900">
+                        4 mins 20 secs
+                    </span>
+
+                    <span class="text-gray-600">
+                        Last Block Reward
+                    </span>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        //
+    }),
+    computed: {
+        //
+    },
+    methods: {
+        //
+    },
+    created: function () {
+        //
+    },
+    mounted: function () {
+        //
+    },
+}
+</script>

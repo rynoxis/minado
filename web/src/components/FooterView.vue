2f58beb41e052f2811695542e88ffd7a211a37b7
rynoxis <goldenpath11090@gmail.com>
2022-08-23 11:36:35 -0400

    Update footer.

---
+++ web/src/components/FooterView.vue
@@ -0,0 +1,35 @@
+<template>
+    <main>
+        <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
+            <div class="border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left">
+                <span class="block sm:inline">
+                    &copy; {{new Date().getFullYear()}} Nexa Rocks!
+                </span>
+
+                <span class="block sm:inline">
+                    All rights reserved.
+                </span>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        //
+    }),
+    computed: {
+        //
+    },
+    methods: {
+        //
+    },
+    created: function () {
+        //
+    },
+    mounted: function () {
+        //
+    },
+}
+</script>
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -211,181 +211,32 @@
 
                     <!-- Right column -->
                     <div class="grid grid-cols-1 gap-4">
-                        <!-- Announcements -->
-                        <section aria-labelledby="announcements-title">
-                            <div class="rounded-lg bg-white overflow-hidden shadow">
-                                <div class="p-6">
-                                    <h2 class="text-base font-medium text-gray-900" id="announcements-title">Announcements</h2>
-                                    <div class="flow-root mt-6">
-                                        <ul role="list" class="-my-5 divide-y divide-gray-200">
-                                            <li class="py-5">
-                                                <div class="relative focus-within:ring-2 focus-within:ring-cyan-500">
-                                                    <h3 class="text-sm font-semibold text-gray-800">
-                                                        <a href="javascript://" class="hover:underline focus:outline-none">
-                                                            <!-- Extend touch target to entire panel -->
-                                                            <span class="absolute inset-0" aria-hidden="true"></span>
-                                                            Office closed on July 2nd
-                                                        </a>
-                                                    </h3>
-                                                    <p class="mt-1 text-sm text-gray-600 line-clamp-2">
-                                                        Cum qui rem deleniti. Suscipit in dolor veritatis sequi aut. Vero ut earum quis deleniti. Ut a sunt eum cum ut repudiandae possimus. Nihil ex tempora neque cum consectetur dolores.
-                                                    </p>
-                                                </div>
-                                            </li>
+                        <BlockRewards />
 
-                                            <li class="py-5">
-                                                <div class="relative focus-within:ring-2 focus-within:ring-cyan-500">
-                                                    <h3 class="text-sm font-semibold text-gray-800">
-                                                        <a href="javascript://" class="hover:underline focus:outline-none">
-                                                            <!-- Extend touch target to entire panel -->
-                                                            <span class="absolute inset-0" aria-hidden="true"></span>
-                                                            New password policy
-                                                        </a>
-                                                    </h3>
-                                                    <p class="mt-1 text-sm text-gray-600 line-clamp-2">
-                                                        Alias inventore ut autem optio voluptas et repellendus. Facere totam quaerat quam quo laudantium cumque eaque excepturi vel. Accusamus maxime ipsam reprehenderit rerum id repellendus
-                                                        rerum. Culpa cum vel natus. Est sit autem mollitia.
-                                                    </p>
-                                                </div>
-                                            </li>
-
-                                            <li class="py-5">
-                                                <div class="relative focus-within:ring-2 focus-within:ring-cyan-500">
-                                                    <h3 class="text-sm font-semibold text-gray-800">
-                                                        <a href="javascript://" class="hover:underline focus:outline-none">
-                                                            <!-- Extend touch target to entire panel -->
-                                                            <span class="absolute inset-0" aria-hidden="true"></span>
-                                                            Office closed on July 2nd
-                                                        </a>
-                                                    </h3>
-                                                    <p class="mt-1 text-sm text-gray-600 line-clamp-2">
-                                                        Tenetur libero voluptatem rerum occaecati qui est molestiae exercitationem. Voluptate quisquam iure assumenda consequatur ex et recusandae. Alias consectetur voluptatibus. Accusamus a ab
-                                                        dicta et. Consequatur quis dignissimos voluptatem nisi.
-                                                    </p>
-                                                </div>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div class="mt-6">
-                                        <a href="javascript://" class="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"> View all </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-
-                        <!-- Recent Hires -->
-                        <section aria-labelledby="recent-hires-title">
-                            <div class="rounded-lg bg-white overflow-hidden shadow">
-                                <div class="p-6">
-                                    <h2 class="text-base font-medium text-gray-900" id="recent-hires-title">Recent Hires</h2>
-                                    <div class="flow-root mt-6">
-                                        <ul role="list" class="-my-5 divide-y divide-gray-200">
-                                            <li class="py-4">
-                                                <div class="flex items-center space-x-4">
-                                                    <div class="flex-shrink-0">
-                                                        <img
-                                                            class="h-8 w-8 rounded-full"
-                                                            src="https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                            alt=""
-                                                        />
-                                                    </div>
-                                                    <div class="flex-1 min-w-0">
-                                                        <p class="text-sm font-medium text-gray-900 truncate">Leonard Krasner</p>
-                                                        <p class="text-sm text-gray-500 truncate">@leonardkrasner</p>
-                                                    </div>
-                                                    <div>
-                                                        <a href="javascript://" class="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"> View </a>
-                                                    </div>
-                                                </div>
-                                            </li>
-
-                                            <li class="py-4">
-                                                <div class="flex items-center space-x-4">
-                                                    <div class="flex-shrink-0">
-                                                        <img
-                                                            class="h-8 w-8 rounded-full"
-                                                            src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                            alt=""
-                                                        />
-                                                    </div>
-                                                    <div class="flex-1 min-w-0">
-                                                        <p class="text-sm font-medium text-gray-900 truncate">Floyd Miles</p>
-                                                        <p class="text-sm text-gray-500 truncate">@floydmiles</p>
-                                                    </div>
-                                                    <div>
-                                                        <a href="javascript://" class="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"> View </a>
-                                                    </div>
-                                                </div>
-                                            </li>
-
-                                            <li class="py-4">
-                                                <div class="flex items-center space-x-4">
-                                                    <div class="flex-shrink-0">
-                                                        <img
-                                                            class="h-8 w-8 rounded-full"
-                                                            src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                            alt=""
-                                                        />
-                                                    </div>
-
-                                                    <div class="flex-1 min-w-0">
-                                                        <p class="text-sm font-medium text-gray-900 truncate">Emily Selman</p>
-                                                        <p class="text-sm text-gray-500 truncate">@emilyselman</p>
-                                                    </div>
-
-                                                    <div>
-                                                        <a href="javascript://" class="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"> View </a>
-                                                    </div>
-                                                </div>
-                                            </li>
-
-                                            <li class="py-4">
-                                                <div class="flex items-center space-x-4">
-                                                    <div class="flex-shrink-0">
-                                                        <img
-                                                            class="h-8 w-8 rounded-full"
-                                                            src="https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                            alt=""
-                                                        />
-                                                    </div>
-                                                    <div class="flex-1 min-w-0">
-                                                        <p class="text-sm font-medium text-gray-900 truncate">Kristin Watson</p>
-                                                        <p class="text-sm text-gray-500 truncate">@kristinwatson</p>
-                                                    </div>
-                                                    <div>
-                                                        <a href="javascript://" class="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"> View </a>
-                                                    </div>
-                                                </div>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div class="mt-6">
-                                        <a href="javascript://" class="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"> View all </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
+                        <RecentPayouts />
                     </div>
                 </div>
             </div>
         </main>
 
-        <footer>
-            <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
-                <div class="border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left"><span class="block sm:inline">&copy; 2021 Tailwind Labs Inc.</span> <span class="block sm:inline">All rights reserved.</span></div>
-            </div>
-        </footer>
+        <FooterView />
     </main>
 </template>
 
 <script>
 /* Import components. */
+import BlockRewards from '@/components/BlockRewards'
+import FooterView from '@/components/FooterView'
 import HeaderView from '@/components/HeaderView'
+import RecentPayouts from '@/components/RecentPayouts'
 import WelcomeView from '@/components/WelcomeView'
 
 export default {
     components: {
+        BlockRewards,
+        FooterView,
         HeaderView,
+        RecentPayouts,
         WelcomeView,
     }
 }

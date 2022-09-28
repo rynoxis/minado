938f4641b9cd842d50f15fc8e78e3006dd7e6e94
rynoxis <goldenpath11090@gmail.com>
2022-09-28 03:52:51 -0400

    Enable magic link admin auth.

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,26 +1,204 @@
 /* Import modules. */
 const moment = require('moment')
+const PouchDB = require('pouchdb')
 const superagent = require('superagent')
+const util = require('util')
+const { v4: uuidv4 } = require('uuid')
+
+const { Magic } = require('@magic-sdk/admin')
+
+const magicAdmin = new Magic(process.env.MAGIC_LINK_KEY)
+
+/* Initialize databases. */
+const logsDb = new PouchDB(`http://${process.env.COUCHDB_AUTH}@localhost:5984/logs`)
+const profilesDb = new PouchDB(`http://${process.env.COUCHDB_AUTH}@localhost:5984/profiles`)
+const sessionsDb = new PouchDB(`http://${process.env.COUCHDB_AUTH}@localhost:5984/sessions`)
 
 /**
  * Administration Module
  */
 const admin = async function (req, res) {
-    let source
+    let account
+    let action
+    let body
+    let createdAt
+    let data
+    let didToken
+    let email
+    let id
+    let issuer
+    let metadata
+    let pkg
+    let results
 
-    if (req.params) {
-        source = req.params.source
+    body = req.body
+    console.log('BODY', body)
+
+    /* Validate body. */
+    if (body) {
+        id = uuidv4()
+        createdAt = moment().unix()
+
+        pkg = {
+            _id: id,
+            src: 'magic',
+            ...body,
+            createdAt,
+        }
+
+        results = await logsDb.put(pkg)
+            .catch(err => console.error('LOGS ERROR:', err))
     }
 
-    if (req.method === 'POST') {
-        console.log('BODY', req.body)
+    /* Set DID token. */
+    didToken = body.didToken
+    // console.log('DID Token', didToken)
 
-        return res.json({ hi: 'there!' })
-    } else {
-        return res.end('done!')
+    if (!didToken) {
+        /* Set status. */
+        res.status(400)
+
+        /* Return error. */
+        return res.json({
+            error: 'Missing DID token.'
+        })
+    }
+
+    /* Set issuer. */
+    issuer = magicAdmin.token.getIssuer(didToken)
+    // console.log('ISSUER', issuer)
+
+    /* Set (public) account/address. */
+    account = magicAdmin.token.getPublicAddress(didToken)
+    // console.log('ACCOUNT', account)
+
+    if (!account) {
+        /* Set status. */
+        res.status(400)
+
+        /* Return error. */
+        return res.json({
+            error: 'Missing user (account) address.'
+        })
+    }
+
+    /* Set issuer metadata. */
+    metadata = await magicAdmin.users.getMetadataByIssuer(issuer)
+    // console.log('MAGIC LOGIN (data):', JSON.stringify(metadata, null, 4))
+
+    /* Set email address. */
+    email = metadata.email
+
+    if (!email) {
+        /* Set status. */
+        res.status(400)
+
+        /* Return error. */
+        return res.json({
+            error: 'Missing user email.'
+        })
+    }
+
+    /* Request existing user. */
+    results = await profilesDb.query('api/byEmail', {
+        key: email,
+        include_docs: true,
+    }).catch(err => {
+        console.error('DATA ERROR:', err)
+    })
+    console.log('PROFILES RESULT (byEmail)', util.inspect(results, false, null, true))
+
+    if (!results || results.rows.length === 0) {
+        id = uuidv4()
+        createdAt = moment().unix()
+
+        pkg = {
+            _id: id,
+            account,
+            email,
+            createdAt,
+            updatedAt: createdAt,
+        }
+
+        /* Retrieve results. */
+        results = await profilesDb.put(pkg)
+            .catch(err => {
+                console.error('PROFILES ERROR:', err)
+            })
+        console.log('RESULT (new profile)', util.inspect(results, false, null, true))
+    }
+
+    /* Set (created) timestamp. */
+    // createdAt = moment().unix()
+
+    /* Build (data) package. */
+    // pkg = {
+    //     _id: uuidv4(),
+    //     account,
+    //     email,
+    //     metadata,
+    //     issuer,
+    //     createdAt,
+    // }
+    // console.log('PKG', pkg)
+
+    /* Retrieve results. */
+    // results = await sessionsDb.put(pkg)
+    //     .catch(err => {
+    //         console.error('AUTH ERROR:', err)
+    //     })
+    // console.log('RESULT (sessions)', util.inspect(results, false, null, true))
+
+    // if (!results) {
+    //     /* Set status. */
+    //     res.status(400)
+
+    //     /* Return error. */
+    //     return res.json([])
+    // }
+
+    action = body.action
+
+    if (!action) {
+        /* Set status. */
+        res.status(400)
+
+        /* Return error. */
+        return res.json({
+            error: 'Missing an action.'
+        })
+    }
+
+    if (action === 'get_profiles') {
+
+        /* Request existing user. */
+        results = await profilesDb.query('api/byAddress', {
+            include_docs: true,
+        }).catch(err => {
+            console.error('DATA ERROR:', err)
+        })
+        console.log('PROFILES RESULT (byEmail)', util.inspect(results, false, null, true))
+
+        /* Validate data. */
+        if (results && results.rows.length !== 0) {
+            /* Map data (doc) results. */
+            data = results.rows.map(_profile => {
+                return _profile.doc
+            })
+        }
 
     }
 
+    /* Build (result) package. */
+    pkg = {
+        data,
+        error: null,
+        success: true,
+        metadata,
+    }
+
+    /* Return params. */
+    res.json(pkg)
 }
 
 /* Export module. */
---
+++ api/shares/server.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const express = require('express')
+const cors = require('cors')
+
+/* Set constants. */
+const HOST = '127.0.0.1'
+const PORT = 4000
+
+/* Initialize application. */
+const app = express()
+
+/* Initialize CORS. */
+app.use(cors())
+
+/* Initialize JSON parser. */
+app.use(express.json())
+
+/* Initialize URL parser. */
+app.use(express.urlencoded({ extended: true }))
+
+/* Build welcome message. */
+const welcome = `
+<html>
+<body>
+
+<h2>Welcome to the Nexa Rocks! API</h2>
+<h3>https://api.nexa.rocks</h3>
+
+</body>
+</html>
+`
+
+// TODO: Replace with a "static" site.
+app.get('/', (req, res) => {
+    res.end(welcome)
+})
+
+/* Initialize Administration route. */
+app.post('/v1/admin', require('./routes/admin'))
+
+/* Initialize Sessions route. */
+app.post('/v1/sessions', require('./routes/sessions'))
+
+/* Initialize Notifications route. */
+app.post('/v1/notifs', require('./routes/shares'))
+
+/* Initialize Magic (Email) Link route. */
+app.post('/v1/magiclink', require('./routes/magiclink'))
+
+// TODO: Offer help.
+app.get('/v1', (req, res) => {
+    res.end('Oops! I think you forgot something.')
+})
+
+/* Start listening for connections. */
+app.listen(PORT, HOST)
+
+/* Display current environment variables. */
+console.info()
+console.log(`Running on http://${HOST}:${PORT}`)
+console.info()
+console.info('Current Environment Variables')
+console.info('-----------------------------')
+console.info('  - NODE_ENV       :', process.env.NODE_ENV)
+console.info('  - COUCHDB_AUTH   :', process.env.COUCHDB_AUTH)
+console.info('  - MAGIC_LINK_KEY :', process.env.MAGIC_LINK_KEY)
+console.info('  - SMTP_HOST      :', process.env.SMTP_HOST)
+console.info('  - SMTP_USER      :', process.env.SMTP_USER)
+console.info('  - SMTP_PASS      :', process.env.SMTP_PASS)
+console.info()

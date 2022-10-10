7fb1d671e8a034989b9b677d94951aed3c2dd8c2
rynoxis <goldenpath11090@gmail.com>
2022-10-10 04:38:11 -0400

    Add payment options.

--- web/src/views/OrderView.vue
+++ web/src/views/OrderView.vue
@@ -134,10 +134,87 @@
                         </form>
 
                         <div class="mt-10 divide-y divide-gray-200 border-t border-b border-gray-200">
-                            <button type="button" disabled class="w-full cursor-auto py-6 text-left text-lg font-medium text-gray-500">Payment details</button>
-                            <button type="button" disabled class="w-full cursor-auto py-6 text-left text-lg font-medium text-gray-500">Shipping address</button>
-                            <button type="button" disabled class="w-full cursor-auto py-6 text-left text-lg font-medium text-gray-500">Billing address</button>
-                            <button type="button" disabled class="w-full cursor-auto py-6 text-left text-lg font-medium text-gray-500">Review</button>
+                            <button type="button" disabled class="w-full cursor-auto py-6 text-left text-lg font-medium text-gray-500">
+                                Stablecoins
+                            </button>
+
+                            <div class="flex gap-4">
+                                <div class="flex flex-col items-center">
+                                    <img
+                                        src="https://assets.telr.io/coins/svg/usdt-ethereum.svg"
+                                        alt="Tether (USDT)"
+                                        class="h-16 w-16 flex-none rounded-md object-cover object-center"
+                                    />
+
+                                    <span class="mt-1 block text-sm text-gray-600">
+                                        Tether (USDT)
+                                    </span>
+
+                                    <span class="mt-0 block text-xs text-gray-400 italic">
+                                        Ethereum
+                                    </span>
+                                </div>
+
+                                <div class="flex flex-col items-center">
+                                    <img
+                                        src="https://assets.telr.io/coins/svg/usdt-ethereum.svg"
+                                        alt="Tether (USDT)"
+                                        class="h-16 w-16 flex-none rounded-md object-cover object-center"
+                                    />
+
+                                    <span class="mt-1 block text-sm text-gray-600">
+                                        Tether (USDT)
+                                    </span>
+
+                                    <span class="mt-0 block text-xs text-gray-400 italic">
+                                        Ethereum
+                                    </span>
+                                </div>
+
+                            </div>
+
+                            <button type="button" disabled class="w-full cursor-auto py-6 text-left text-lg font-medium text-gray-500">
+                                Coins
+                            </button>
+
+                            <div class="flex gap-4">
+                                <div class="flex flex-col items-center">
+                                    <img
+                                        src="https://assets.telr.io/coins/svg/btc.svg"
+                                        alt="Bitcoin (BTC)"
+                                        class="h-16 w-16 flex-none rounded-md object-cover object-center"
+                                    />
+
+                                    <span class="mt-1 block text-sm text-gray-600">
+                                        Bitcoin (BTC)
+                                    </span>
+                                </div>
+
+                                <div class="flex flex-col items-center">
+                                    <img
+                                        src="https://assets.telr.io/coins/svg/btc.svg"
+                                        alt="Tether (USDT)"
+                                        class="h-16 w-16 flex-none rounded-md object-cover object-center"
+                                    />
+
+                                    <span class="mt-1 block text-sm text-gray-600">
+                                        Tether
+                                    </span>
+
+                                    <span class="mt-0 block text-xs text-gray-400 italic">
+                                        Ethereum
+                                    </span>
+                                </div>
+
+                            </div>
+
+                            <button type="button" disabled class="w-full cursor-auto py-6 text-left text-lg font-medium text-gray-500">
+                                Tokens
+                            </button>
+
+                            <button type="button" disabled class="w-full cursor-auto py-6 text-left text-lg font-medium text-gray-500">
+                                Review
+                            </button>
                         </div>
                     </div>
                 </div>

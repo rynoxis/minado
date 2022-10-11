fd3ed478fec233d2d0011d55b71f3f61bc961115
rynoxis <goldenpath11090@gmail.com>
2022-10-11 06:33:46 -0400

    Enable NL daemon.

--- api/daemon/index.js
+++ api/daemon/index.js
@@ -9,234 +9,44 @@ const { v4: uuidv4 } = require('uuid')
 /* Initialize databases. */
 // const logsDb = new PouchDB(`http://${process.env.COUCHDB_AUTH}@localhost:5984/logs`)
 // const sessionsDb = new PouchDB(`http://${process.env.COUCHDB_AUTH}@localhost:5984/sessions`)
-const notifsDb = new PouchDB('dbs/notifs')
-const sessionsDb = new PouchDB('dbs/sessions')
 
 /* Initialize new Bitcoin client. */
-const client = new Client({
-    port: process.env.NEXA_RPC_PORT || 7227, // Testnet RPC port is 7229
-    host: process.env.NEXA_RPC_HOST || '127.0.0.1',
-    username: process.env.NEXA_RPC_USER || 'user',
-    password: process.env.NEXA_RPC_PASS || 'password',
-})
-
+// const client = new Client({
+//     port: process.env.NEXA_RPC_PORT || 7227, // Testnet RPC port is 7229
+//     host: process.env.NEXA_RPC_HOST || '127.0.0.1',
+//     username: process.env.NEXA_RPC_USER || 'user',
+//     password: process.env.NEXA_RPC_PASS || 'password',
+// })
 
 
 /**
- * Notifications
+ * Runner
  */
-const notifs = async function (req, res) {
+const run = async function () {
     let id
     let address
     let body
     let response
     let results
 
-    body = req.body
-    console.log('BODY', body)
-
-    /* Validate body. */
-    if (!body) {
-        /* Set status. */
-        res.status(400)
-
-        /* Return error. */
-        return res.json({
-            error: 'Missing body parameter.'
-        })
+    /**
+     * Handle Notifications
+     */
+    _handleNotifs = () => {
+        console.log('handling notifications')
     }
 
-    address = body.address
-    console.log('\nNotification address:', address)
+    /* Initialize 5-second intervals. */
+    setInterval(() => {
+        _handleNotifs()
+    }, 5000)
+    
 
-    result = await client.validateAddress(address)
-    console.log('\nIs address valid:', result.isvalid, result)
-
-    /* Validate address. */
-    if (!result.isvalid) {
-        /* Set status. */
-        res.status(400)
-
-        /* Return error. */
-        return res.json({
-            error: 'Your Nexa address is invalid.'
-        })
-    }
-
-
-
-/*
-
-addmultisigaddress
-
-backupwallet
-
-bumpfee
-clearbanned
-
-combinepsbt
-combinerawtransaction
-converttopsbt
-
-createmultisig
-createpsbt
-createrawtransaction
-
-decodepsbt
-decoderawtransaction
-decodescript
-
-deriveaddresses
-
-dumpprivkey
-dumptxoutset
-dumpwallet
-
-encryptwallet
-estimatefee
-estimatepriority
-estimatesmartfee
-estimatesmartpriority
-
-finalizepsbt
-fundrawtransaction
-
-getaccount
-getaccountaddress
-getaddressinfo
-getbalance
-getbalances
-
-getbestblockhash
-getblockcount
-getblockfilter
-getblockhash
-getblockheader
-getblockstats
-getblocktemplate
-getblockchaininfo
-
-getchaintips
-getchaintxstats
-getdifficulty
-gethashespersec
-
-getindexinfo
-getinfo
-getmemoryinfo
-
-getmempoolancestors
-getmempooldescendants
-getmempoolentry
-getmempoolinfo
-
-getmininginfo
-
-getnettotals
-getnetworkhashps
-getnetworkinfo
-
-getnewaddress
-getnodeaddresses
-getpeerinfo
-
-getrawchangeaddress
-getrawmempool
-getrawtransaction
-getreceivedbyaccount
-getreceivedbyaddress
-getreceivedbylabel
-
-getrpcinfo
-gettransaction
-gettxout
-gettxoutproof
-gettxoutsetinfo
-
-getunconfirmedbalance
-getwalletinfo
-
-getwork
-getzmqnotifications
-
-importaddress
-importmulti
-importprivkey
-importprunedfunds
-importpubkey
-importwallet
-
-joinpsbts
-
-keypoolrefill
-
-listaccounts
-listaddressgroupings
-listbanned
-listlabels
-
-listlockunspent
-listreceivedbyaccount
-listreceivedbyaddress
-listreceivedbylabel
-listsinceblock
-listtransactions
-
-listunspent
-listwalletdir
-
-listwallets
-loadwallet
-
-lockunspent
-
-preciousblock
-prioritisetransaction
-pruneblockchain
-
-scantxoutset
-sendfrom
-sendmany
-sendrawtransaction
-sendtoaddress
-
-sethdseed
-setlabel
-
-setnetworkactive
-settxfee
-setwalletflag
-
-signmessage
-signmessagewithprivkey
-signrawtransaction
-signrawtransactionwithkey
-signrawtransactionwithwallet
-
-submitblock
-testmempoolaccept
-
-unloadwallet
-uptime
-utxoupdatepsbt
-
-validateaddress
-verifychain
-verifymessage
-verifytxoutproof
-walletcreatefundedpsbt
-walletlock
-walletpassphrase
-walletpassphrasechange
-walletprocesspsbt
-
-*/
-
-    const balance = await client.getBalance('*', 0)
-    console.log('\nBALANCE', balance)
+    // const balance = await client.getBalance('*', 0)
+    // console.log('\nSERVER BALANCE', balance)
 
     // client.getInfo().then((help) => console.log(help))
-return res.json({ status: 'done!', balance, })
+return
 
     /* Generate id. */
     id = uuidv4()
@@ -290,3 +100,5 @@ return res.json({ status: 'done!', balance, })
         messageId: info.messageId,
     })
 }
+
+run()

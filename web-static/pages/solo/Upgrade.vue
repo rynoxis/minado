17bcaa55f4174e2ad2d6f01b5bb52a7450815486
rynoxis <goldenpath11090@gmail.com>
2022-10-14 02:53:35 -0400

    Add solo mining upgrade page.

---
+++ web-static/pages/solo/Upgrade.vue
@@ -0,0 +1,55 @@
+<template>
+    <main class="min-h-full">
+        <HeaderView />
+
+        <section class="-mt-24 pb-8">
+            <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                <h1 class="sr-only">Upgrade Your Nexa Miners</h1>
+
+                <!-- Main 3 column grid -->
+                <div class="grid grid-cols-1 gap-4 items-start lg:grid-cols-3 lg:gap-8">
+                    <section class="col-span-2">
+                        <SoloUpgrade />
+                    </section>
+
+                    <!-- Right column -->
+                    <div class="grid grid-cols-1 gap-4">
+                        <PayoutsPanel />
+
+                        <BlockRewardsPanel />
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <FooterView />
+    </main>
+</template>
+
+<script>
+/* Import components. */
+import BlockRewardsPanel from '@/components/BlockRewardsPanel'
+import PayoutsPanel from '@/components/PayoutsPanel'
+
+export default {
+    components: {
+        BlockRewardsPanel,
+        PayoutsPanel
+    },
+    data: () => ({
+        //
+    }),
+    computed: {
+        //
+    },
+    methods: {
+        //
+    },
+    created: function () {
+        //
+    },
+    mounted: function () {
+        //
+    }
+}
+</script>

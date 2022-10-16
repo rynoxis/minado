21293f2eeb8b74308d037b8fe829918fd1267c3a
rynoxis <goldenpath11090@gmail.com>
2022-10-15 23:42:10 -0400

    Escape to close panel.

--- web-static/components/SidePanel.vue
+++ web-static/components/SidePanel.vue
@@ -113,7 +113,20 @@ export default {
         this.tab = 'profile'
     },
     mounted: function () {
-        //
+        document.onkeydown = (evt) => {
+            evt = evt || window.event
+            let isEscape = false
+
+            if ('key' in evt) {
+                isEscape = (evt.key === 'Escape' || evt.key === 'Esc')
+            } else {
+                isEscape = (evt.keyCode === 27)
+            }
+
+            if (isEscape) {
+                this.closePanel()
+            }
+        }
     }
 }
 </script>

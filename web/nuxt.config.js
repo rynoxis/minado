4ba5dfe3a5932bf171652e5d17576abec0993517
rynoxis <goldenpath11090@gmail.com>
2022-11-03 00:27:20 -0400

    Add Docker files.

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -2,6 +2,11 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  server: {
+    host: process.env.NUXT_HOST,
+    port: process.env.NUXT_PORT,
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Nexa Rocks! Solo, Pool & Cloud Mining NEXA Assets',

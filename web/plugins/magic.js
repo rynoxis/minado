769cd1bcadbaf19ba82a38155f41967aad7c6d48
rynoxis <goldenpath11090@gmail.com>
2022-11-09 17:13:27 -0500

    Update magic key.

--- web/plugins/magic.js
+++ web/plugins/magic.js
@@ -5,4 +5,5 @@ const createMagic = (key) => {
     return typeof window !== 'undefined' && new Magic(key)
 }
 
-export const magic = createMagic(process.env.NUXT_ENV_MAGIC_PUBLISHABLE_KEY)
+// export const magic = createMagic(process.env.NUXT_ENV_MAGIC_PUBLISHABLE_KEY)
+export const magic = createMagic('pk_live_CAF7378F498C1F81') // Ava's DAO

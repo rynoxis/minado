4a73773172df7d324f661ea501cd725b885dbf9a
rynoxis <goldenpath11090@gmail.com>
2023-03-24 19:30:54 -0400

    Change to arrays.

--- graphql/src/queries/affiliate.js
+++ graphql/src/queries/affiliate.js
@@ -1,24 +1,28 @@
 /* Import types. */
 import AffiliateType from '../types/Affiliate.js'
 
+const SAMPLE_AFFILIATE = {
+    affiliateid: '4ab47638-ba1b-4121-af48-39b2ca2c52f8',
+    shortid: 'abc123',
+    rewards: {
+        rewardid: 'fb73571c-b4c8-4489-a199-454e719b78bf',
+        affiliateid: '4ab47638-ba1b-4121-af48-39b2ca2c52f8',
+        amount: 1337,
+        createdAt: 1679699865,
+    },
+    createdAt: 1679699791,
+}
+
 /**
  * Affiliate
  */
 export default {
     type: AffiliateType,
     resolve: (parent, args, params) => {
+        console.log('BLANK PARENT:', parent)
+        console.log('BLANK ARGS:', args)
         console.log('BLANK PARAMS:', params)
-        return {
-            affiliateid: '4ab47638-ba1b-4121-af48-39b2ca2c52f8',
-            shortid: 'abc123',
-            rewards: {
-                rewardid: 'fb73571c-b4c8-4489-a199-454e719b78bf',
-                affiliateid: '4ab47638-ba1b-4121-af48-39b2ca2c52f8',
-                amount: 1337,
-                createdAt: 1679699865,
-            },
-            createdAt: 1679699791,
-        }
+        return [SAMPLE_AFFILIATE]
     },
     description: `Request Affiliate program details, including: balances, bonuses and more...`,
 }
